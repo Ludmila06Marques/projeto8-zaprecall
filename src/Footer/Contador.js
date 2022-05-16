08dcfefc881bdb1ba6c434053f9f87dea9b6442d
Ludmila Marques <102738642+Ludmila06Marques@users.noreply.github.com>
2022-05-16 12:12:09 +0200

    Update Contador.js

--- src/Footer/Contador.js
+++ src/Footer/Contador.js
@@ -1,12 +1,12 @@
 
 
-export default function Contador({iconeColor , iconeType ,respondidas }){
+export default function Contador({iconeColor , iconeType ,array }){
     return( 
         <div className="contador">
             <div className="icones">
             <ion-icon className={iconeColor} name={iconeType}></ion-icon>          
             </div>
-            <h3>{respondidas.length}/8</h3>
+            <h3>{array.length}/8</h3>
             </div>
     )
 }
